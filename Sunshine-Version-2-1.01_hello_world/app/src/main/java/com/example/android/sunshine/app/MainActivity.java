4e471c72e3b7bb9cac4a83479f2725c6d2dd8090
yehuda ornstein <yehuda ornstein>
2016-07-04 20:10:31 -0400

    add list view, add adapter, add fate data

--- Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +61,23 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> fakeData = new ArrayList<String>();
+
+            fakeData.add("Today-Sunny-88/63");
+            fakeData.add("Tomorrow-Foggy-70/46");
+            fakeData.add("Wed-Cloudy-73/63");
+            fakeData.add("Thurs-Rainy-64/51");
+            fakeData.add("Friday-Foggy-70/46");
+            fakeData.add("Say-sunny-76/68");
+            fakeData.add("Sun-Awesome day");
+
+            ArrayAdapter<String> myAdapt = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textView,fakeData);
+
+            ListView lv = (ListView) rootView.findViewById(R.id.list_view_forecast);
+
+            lv.setAdapter(myAdapt);
+
             return rootView;
         }
     }
